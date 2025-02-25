f1e32b4a1862e83f9e7ad870a19da2648f13cddc
msan222 <madelinesanvictores@gmail.com>
2025-02-24 18:16:03 -0800

    cont basic

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"net/http"
 )
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8 ")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	mux := http.NewServeMux()
+
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
+	mux.HandleFunc("/healthz", healthzHandler)
 
 	server := &http.Server{
 		Handler: mux,
